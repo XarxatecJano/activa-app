cd273ce336f10d240aa9d89842bf14514fad8698
Jano Soler <jano.soler@xarxatecactiva.com>
2022-10-25 10:09:15 +0200

    added /procesa-formulario route logic

--- build/handlers/contacFormProcessUtilities.js
+++ build/handlers/contacFormProcessUtilities.js
@@ -3,7 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.contactFormProcessUtilities = void 0;
 class contactFormProcessUtilities {
     static sendResponseToBrowserAfterSubmit(req, res) {
-        res.send("Hola");
+        const expRegMail = /.+@xarxatecactiva\.com$/;
+        if (expRegMail.test(req.body.emailAddress)) {
+            res.send(`<p>Hola, soy ${req.body.name}</p>`);
+        }
+        else {
+            res.send(`<p>No es un mail válido</p>`);
+        }
     }
 }
 exports.contactFormProcessUtilities = contactFormProcessUtilities;
--- src/handlers/contacFormProcessUtilities.ts
+++ src/handlers/contacFormProcessUtilities.ts
@@ -2,6 +2,12 @@ import express from 'express';
 
 export class contactFormProcessUtilities {
     static sendResponseToBrowserAfterSubmit(req: express.Request, res: express.Response){
-        res.send("Hola");
+        const expRegMail = /.+@xarxatecactiva\.com$/;
+        if (expRegMail.test(req.body.emailAddress)){
+            res.send(`<p>Hola, soy ${req.body.name}</p>`);
+        } else {
+            res.send(`<p>No es un mail válido</p>`);
+        }
+
     }
 }
